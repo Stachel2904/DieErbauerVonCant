3a691eae8c4ca277a0171d33153301b489e0cfb9
Jan-Vincent Mock <11009799@stud.hochschule-heidelberg.de>
2018-11-24 16:37:55 +0100

    Serverlist zeigt nun den Servernamen und un Portfehler abgefangen

--- Die Erbauer von Cant/Assets/Scripts/Network/Client/NetworkClientDiscovery.cs
+++ Die Erbauer von Cant/Assets/Scripts/Network/Client/NetworkClientDiscovery.cs
@@ -4,7 +4,7 @@ using UnityEngine.Networking;
 public class NetworkClientDiscovery : NetworkDiscovery {
 
     public string ipAddress;
-    public string serverMessage;
+    public string serverName;
     public int port;
 
     public void StartClientDiscovery() {
@@ -13,10 +13,14 @@ public class NetworkClientDiscovery : NetworkDiscovery {
     }
     public override void OnReceivedBroadcast(string _fromAddress_, string _data_){
         ipAddress = _fromAddress_.Substring(_fromAddress_.LastIndexOf(":") + 1, _fromAddress_.Length - (_fromAddress_.LastIndexOf(":") + 1));
-        serverMessage = _data_.Substring(_data_.LastIndexOf(":") + 1, _data_.Length - (_data_.LastIndexOf(":") + 1));
+        string serverMessage = _data_.Substring(_data_.LastIndexOf(":") + 1, _data_.Length - (_data_.LastIndexOf(":") + 1));
         Debug.Log(serverMessage);
-        string s_port = _data_.Substring(_data_.LastIndexOf("|") + 1, _data_.Length - (_data_.LastIndexOf("|") + 1));
-        int.TryParse(s_port, out port);
+        string[] deltas = serverMessage.Split('|');
+        serverName = deltas[0];
+        string s_port = deltas[1];
+        if (!int.TryParse(s_port, out port)) {
+            Debug.LogError("Can not parse port out of serverMessage!");
+        }
         Debug.Log(ipAddress + " / " + port);
         this.gameObject.GetComponent<NetworkClientGUI>().OutputAdresses();
     }
