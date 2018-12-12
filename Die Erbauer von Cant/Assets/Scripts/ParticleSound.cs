afaae7e4b792abfd3f44891114a33c7af5a6af40
Jan-Vincent Mock <11009799@stud.hochschule-heidelberg.de>
2018-12-12 14:56:21 +0100

    ParticleSound Script, Water, Dings

---
+++ Die Erbauer von Cant/Assets/Scripts/ParticleSound.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ParticleSound : MonoBehaviour {
+
+    private ParticleSystem parSystem;
+    private int numberOfParticles = 0;
+
+    private void Start() {
+        parSystem = this.gameObject.GetComponent<ParticleSystem>();
+    }
+
+    private void Update () {
+        int count = parSystem.particleCount;
+        if(count < numberOfParticles) {
+            //play Sound if particle has died
+        }else if(count > numberOfParticles) {
+            //play Sound if particle is born
+        }
+        numberOfParticles = count;
+	}
+}
